08075f0c3638dd0951952ad630474808377c7221
Robert Recalo <rrecalo3@ggc.edu>
2023-08-24 14:12:24 -0400

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by Robert Recalo on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
